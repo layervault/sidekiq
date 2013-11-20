ad3c8684d530313f9a4244e5469826cdf9b25375
Ryan LeFevre <meltingice8917@gmail.com>
2013-11-20 15:42:27 -0500

    Enable all extensions with renamed methods

--- lib/sidekiq.rb
+++ lib/sidekiq.rb
@@ -119,7 +119,7 @@ module Sidekiq
 
 end
 
-# require 'sidekiq/extensions/class_methods'
+require 'sidekiq/extensions/class_methods'
 require 'sidekiq/extensions/action_mailer'
-# require 'sidekiq/extensions/active_record'
+require 'sidekiq/extensions/active_record'
 require 'sidekiq/rails' if defined?(::Rails::Engine)
--- lib/sidekiq/extensions/active_record.rb
+++ lib/sidekiq/extensions/active_record.rb
@@ -21,13 +21,13 @@ module Sidekiq
     end
 
     module ActiveRecord
-      def delay(options={})
+      def sidekiq_delay(options={})
         Proxy.new(DelayedModel, self, options)
       end
-      def delay_for(interval, options={})
+      def sidekiq_delay_for(interval, options={})
         Proxy.new(DelayedModel, self, options.merge('at' => Time.now.to_f + interval.to_f))
       end
-      def delay_until(timestamp, options={})
+      def sidekiq_delay_until(timestamp, options={})
         Proxy.new(DelayedModel, self, options.merge('at' => timestamp.to_f))
       end
     end
